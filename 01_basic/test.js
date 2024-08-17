208b360ff60ffc7c8066c3337dde98bfc123f7ed
Sandesh Neupane <148778936+codesandesh@users.noreply.github.com>
2024-08-17 09:33:23 +0000

    data type

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,10 @@
 console.log('my name is  sandesh')
+let name1='sandesh'
+const id=123
+var house='red'
+// console.log(name1)
+// console.log(id)
+// console.log(house)  
+/* we can also display this things like this but if we want to print the  this things without the repetation of code we can simply use the 
+table  method for example  */
+console.table([name1,id,house ])
